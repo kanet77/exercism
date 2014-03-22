f9d1e4b39195cdf0f49c0a7634463472771690e1
tk <kexf7pqsdu@snkmail.com>
2014-03-22 17:58:00 -0500

    word-count

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,8 +1,8 @@
 class Phrase
   attr_accessor :word_count
 
-  def initialize(sentence)
-    words = sentence.downcase.scan(/[\w\']+/)
+  def initialize(phrase)
+    words = phrase.downcase.scan(/[\w\']+/)
     counts = Hash.new { |h,k| h[k] = 0 }
     words.each { |word| counts[word] += 1 }
     @word_count = counts
