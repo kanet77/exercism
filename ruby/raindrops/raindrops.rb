ed14f6fdef35a4cc8220df37c73c5cb370f0c9ee
tk <kexf7pqsdu@snkmail.com>
2014-03-22 19:10:42 -0500

    raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,13 +1,9 @@
 class Raindrops
-  def self.is_divisible_by?(num, factor)
-    num % factor == 0
-  end
-
   def self.convert(num)
     response = ''
-    response << 'Pling' if is_divisible_by?(num, 3)
-    response << 'Plang' if is_divisible_by?(num, 5)
-    response << 'Plong' if is_divisible_by?(num, 7)
+    response << 'Pling' if (num % 3).zero?
+    response << 'Plang' if (num % 5).zero?
+    response << 'Plong' if (num % 7).zero?
     return response unless response.empty?
     num.to_s
   end
