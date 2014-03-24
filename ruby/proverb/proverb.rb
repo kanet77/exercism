3c0caec9e5175f663fb50532313827a61bf28036
tk <kexf7pqsdu@snkmail.com>
2014-03-24 01:23:35 -0500

    proverb

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -4,11 +4,11 @@ class Proverb
   def initialize(*lost_things, qualifier: "")
     @saying = ""
 
-    lost_things.each_cons(2) do |things|
-      @saying << "For want of a #{things[0]} the #{things[1]} was lost.\n"
+    lost_things.each_cons(2) do |want, lost|
+      @saying << "For want of a #{want} the #{lost} was lost.\n"
     end
     qualifier << " " unless qualifier.empty?
-    @saying << "And all for the want of a #{qualifier}#{lost_things[0]}."
+    @saying << "And all for the want of a #{qualifier}#{lost_things.first}."
   end
 
   def to_s
