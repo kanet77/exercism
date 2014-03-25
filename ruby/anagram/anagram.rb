622b86a8d6e9b5704d98c2fb1aa56f0f6e6feb02
tk <kexf7pqsdu@snkmail.com>
2014-03-25 01:40:50 -0500

    anagram

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,14 +1,21 @@
 class Anagram
   def initialize(word_to_match)
     @pattern_to_match = pattern(word_to_match)
-    @downcase_match = word_to_match.downcase
+    @original_word = word_to_match.downcase
   end
 
   def match(candidates)
     candidates.select do |c|
-      pattern(c) == @pattern_to_match &&
-      c.downcase != @downcase_match
+      matches_pattern?(c) && is_not_original_word?(c)
     end
+  end
+
+  def matches_pattern?(candidate)
+    pattern(candidate) == @pattern_to_match
+  end
+
+  def is_not_original_word?(candidate)
+    candidate.downcase != @original_word
   end
 
   def pattern(word)
