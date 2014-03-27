90b8945600ff6c0633de101f923182287378d694
Tom Kane <tom.kane@evsr.com>
2014-03-27 00:54:28 -0500

    roman-numerals

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,5 +1,19 @@
 class Integer
-  ROMAN_NUMERALS = {
+  def to_roman
+    digits_with_exp.each_with_object('') do |(digit,exponent),result|
+      result << Integer::digit_to_roman(digit,exponent)
+    end
+  end
+
+private
+
+  def digits_with_exp
+    digits = self.to_s
+    positions = (digits.length - 1).downto(0)
+    digits.chars.zip(positions)
+  end
+
+  ROMAN = {
     1000 => 'M',
     500 => 'D',
     100 => 'C',
@@ -9,37 +23,17 @@ class Integer
     1 => 'I'  
   }
 
-  def to_roman
-    decimal = self
-    roman_base = 1000
-    result = ''
-    while (decimal >= 1) do
-      factor = decimal/roman_base
-      result << Integer::multiple_of(factor, roman_base)
-      decimal -= (factor*roman_base)
-      roman_base /= 10
-    end
-    result
-  end
-
-private
-
-  def self.multiple_of(factor, roman_base)
+  def self.digit_to_roman(digit, exponent)
+    power_of_ten = 10 ** exponent
+    factor = digit.to_i
     case factor
-    when 0
-      ''
-    when 1..3
-      ROMAN_NUMERALS[roman_base]*factor
-    when 4
-      ROMAN_NUMERALS[roman_base]+ROMAN_NUMERALS[roman_base*5]
-    when 5
-      ROMAN_NUMERALS[roman_base*5]
-    when 6..8
-      ROMAN_NUMERALS[roman_base*5]+(ROMAN_NUMERALS[roman_base]*(factor-5))
-    when 9
-      ROMAN_NUMERALS[roman_base] + ROMAN_NUMERALS[roman_base*10]
-    else
-      raise "Nonsense! Factor is #{factor}. Should be in (0..9)."
+    when 0      then ''
+    when 1..3   then ROMAN[power_of_ten] * factor
+    when 4      then ROMAN[power_of_ten] + ROMAN[power_of_ten * 5]
+    when 5      then ROMAN[power_of_ten * 5]
+    when 6..8   then ROMAN[power_of_ten * 5] + ( ROMAN[power_of_ten] * (factor - 5) )
+    when 9      then ROMAN[power_of_ten] + ROMAN[power_of_ten * 10]
+    else        raise "Nonsense! Factor is #{factor}. Should be in (0..9)."
     end
   end
 end
