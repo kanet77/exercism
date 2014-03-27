8f79beef83522ad18206087e9c28df405bd4dd50
Tom Kane <tom.kane@evsr.com>
2014-03-27 00:54:28 -0500

    roman

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,5 +1,31 @@
 class Integer
-  ROMAN_NUMERALS = {
+  def to_roman
+    digits_with_exp.each_with_object('') do |(digit,exponent),result|
+      result << Integer::digit_to_roman(digit,exponent)
+    end
+  end
+
+  # def to_roman
+  #   decimal = self
+  #   power_of_ten = 1000
+  #   result = ''
+  #   while (decimal >= 1) do
+  #     digit = decimal/power_of_ten
+  #     result << Integer::digit_to_roman(digit, power_of_ten)
+  #     decimal -= (digit * power_of_ten)
+  #     power_of_ten /= 10
+  #   end
+  #   result
+  # end
+
+private
+
+  def digits_with_exp
+    digits = self.to_s
+    digits.chars.zip((digits.length - 1).downto(0))
+  end
+
+  ROMAN = {
     1000 => 'M',
     500 => 'D',
     100 => 'C',
@@ -9,35 +35,22 @@ class Integer
     1 => 'I'  
   }
 
-  def to_roman
-    decimal = self
-    roman_base = 1000
-    result = ''
-    while (decimal >= 1) do
-      factor = decimal/roman_base
-      result << Integer::multiple_of(factor, roman_base)
-      decimal -= (factor*roman_base)
-      roman_base /= 10
-    end
-    result
-  end
-
-private
-
-  def self.multiple_of(factor, roman_base)
+  def self.digit_to_roman(digit, exponent)
+    power_of_ten = 10 ** exponent
+    factor = digit.to_i
     case factor
     when 0
       ''
     when 1..3
-      ROMAN_NUMERALS[roman_base]*factor
+      ROMAN[power_of_ten] * factor
     when 4
-      ROMAN_NUMERALS[roman_base]+ROMAN_NUMERALS[roman_base*5]
+      ROMAN[power_of_ten] + ROMAN[power_of_ten * 5]
     when 5
-      ROMAN_NUMERALS[roman_base*5]
+      ROMAN[power_of_ten * 5]
     when 6..8
-      ROMAN_NUMERALS[roman_base*5]+(ROMAN_NUMERALS[roman_base]*(factor-5))
+      ROMAN[power_of_ten * 5] + ( ROMAN[power_of_ten] * (factor - 5) )
     when 9
-      ROMAN_NUMERALS[roman_base] + ROMAN_NUMERALS[roman_base*10]
+      ROMAN[power_of_ten] + ROMAN[power_of_ten * 10]
     else
       raise "Nonsense! Factor is #{factor}. Should be in (0..9)."
     end
