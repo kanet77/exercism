afd22a8ff5440e303b703830e322778344de4ba3
tk <kexf7pqsdu@snkmail.com>
2014-03-27 23:36:04 -0500

    roman

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -10,34 +10,39 @@ class Integer
   }
 
   def to_roman
-    decimal = self
-    roman_base = 1000
-    result = ''
-    while (decimal >= 1) do
-      factor = decimal/roman_base
-      result << Integer::multiple_of(factor, roman_base)
-      decimal -= (factor*roman_base)
-      roman_base /= 10
-    end
-    result
+    digits = self.to_s.chars.to_i
+    digits_with_exp = digits.reverse.each_with_index.to_h.sort_by { |k,v| v }.reverse!
+    digits_with_exp = digits.reverse.each_with_index { |d,i| i,d }.reverse!
+    roman = digits.map(multiple_of)
+    # decimal = self
+    # power_of_ten = ROMAN_NUMERALS.first
+    # result = ''
+    # while (decimal >= 1) do
+    #   factor = decimal/power_of_ten
+    #   result << Integer::multiple_of(factor, power_of_ten)
+    #   decimal -= (factor * power_of_ten)
+    #   power_of_ten /= 10
+    # end
+    # result
   end
 
 private
 
-  def self.multiple_of(factor, roman_base)
-    case factor
+  def self.multiple_of(factor, exponent)
+    power_of_ten = 10 ** exponent
+    case factor.to_i
     when 0
       ''
     when 1..3
-      ROMAN_NUMERALS[roman_base]*factor
+      ROMAN_NUMERALS[power_of_ten] * factor
     when 4
-      ROMAN_NUMERALS[roman_base]+ROMAN_NUMERALS[roman_base*5]
+      ROMAN_NUMERALS[power_of_ten] + ROMAN_NUMERALS[power_of_ten * 5]
     when 5
-      ROMAN_NUMERALS[roman_base*5]
+      ROMAN_NUMERALS[power_of_ten * 5]
     when 6..8
-      ROMAN_NUMERALS[roman_base*5]+(ROMAN_NUMERALS[roman_base]*(factor-5))
+      ROMAN_NUMERALS[power_of_ten * 5] + ( ROMAN_NUMERALS[power_of_ten] * (factor - 5) )
     when 9
-      ROMAN_NUMERALS[roman_base] + ROMAN_NUMERALS[roman_base*10]
+      ROMAN_NUMERALS[power_of_ten] + ROMAN_NUMERALS[power_of_ten * 10]
     else
       raise "Nonsense! Factor is #{factor}. Should be in (0..9)."
     end
