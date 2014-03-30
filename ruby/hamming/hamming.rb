b3de12912eec7d1e9aef866146fc11cafb04cc78
tk <kexf7pqsdu@snkmail.com>
2014-03-30 15:23:23 -0500

    hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,7 +3,7 @@ class Hamming
     return 0 if strand1 == strand2
     dist = 0
     len = [strand1.length, strand2.length].min
-    for n in (0..len-1)
+    (0..len-1).each do |n|
       dist += 1 if strand1[n] != strand2[n]
     end
     dist
