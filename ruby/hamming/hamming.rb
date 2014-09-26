0f4f3292947f7723ab787ffcd9db3b53af76c185
Tom Kane <kexf7pqsdu@snkmail.com>
2014-09-26 14:25:18 -0500

    hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,19 @@
+require 'awesome_print'
+
 class Hamming
-  def self.compute(strand1, strand2)
-    return 0 if strand1 == strand2
-    dist = 0
-    [strand1.length, strand2.length].min.times.each do |n|
-      dist += 1 if strand1[n] != strand2[n]
-    end
-    dist
+  def self.compute(dna1, dna2)
+    return 0 if dna1 == dna2
+
+    nucleotide_pairs(dna1, dna2).count { |n1, n2|
+      n1 != n2
+    }
+  end
+
+  def self.nucleotide_pairs(dna1, dna2)
+    # get corresponding pairs of nucleotides
+    pairs = dna1.chars.zip(dna2.chars)
+    
+    # only compare up to length of shortest DNA strand
+    pairs = pairs.first(dna2.length)
   end
 end
